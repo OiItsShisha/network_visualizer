c37993b1d3de71614756e1ad77dfa02f0fef04e4
Shisha <johns.blake921@gmail.com>
2024-03-04 17:47:22 -0600

    Initial commit, sniffer and base vis working

---
+++ network_visualizer/monitor_network.py
@@ -0,0 +1,16 @@
+import pyshark
+
+def read_cap() -> None:
+    cap = pyshark.FileCapture(".\\home_wifi.pcapng")
+    nx_data = {
+        "comms_tuples": [],
+        "protocol_stack": []
+        }
+    for packet in cap:
+        nx_data["comms_tuples"].append(
+            (packet.eth.src, packet.eth.dst))
+        nx_data["protocol_stack"].append(packet.frame_info.protocols)
+    return nx_data
+if __name__ == "__main__":
+    dict_ = read_cap()
+    print(df)
