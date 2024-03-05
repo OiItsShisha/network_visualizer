6b177d42592a446b9bf17773845452542a1a85ee
shisha <shisha@Ubuntu2204.myguest.virtualbox.org>
2024-03-04 20:00:09 -0600

    got multi edge working between two nodes

--- network_visualizer/monitor_network.py
+++ network_visualizer/monitor_network.py
@@ -1,7 +1,7 @@
 import pyshark
 
 def read_cap() -> None:
-    cap = pyshark.FileCapture(".\\home_wifi.pcapng")
+    cap = pyshark.FileCapture("./home_wifi.pcapng")
     nx_data = {
         "comms_tuples": [],
         "protocol_stack": []
@@ -11,6 +11,7 @@ def read_cap() -> None:
             (packet.eth.src, packet.eth.dst))
         nx_data["protocol_stack"].append(packet.frame_info.protocols)
     return nx_data
+
 if __name__ == "__main__":
     dict_ = read_cap()
-    print(df)
+    print(dict_)
--- network_visualizer/network_vis.py
+++ network_visualizer/network_vis.py
@@ -1,20 +1,37 @@
 import networkx as nx
 from monitor_network import read_cap
 from pyvis.network import Network
+import matplotlib.pyplot as plt
 
 def make_nodes(net, data):
     """ Method for making the nodes on the graph"""
     for coms, protos in zip(data["comms_tuples"], data["protocol_stack"]):
-        net.add_node(coms[0]) # add src_addr
-        net.add_node(coms[1]) # add dst_addr
-        net.add_edge(coms[0], coms[1], value=protos)
-        print(net.nodes)
-        exit()
-    net.show(".\\test.html", notebook=False)
+        # If the graph is empty make the first nodes / edges
+        proto = protos.split(":")[-1]
+        if not net.edges:
+            # net.add_edge adds nodes if they don't exist
+            net.add_edge(coms[0], coms[1], value=proto)
+        else:
+            if net.has_edge(coms[0], coms[1]):
+                edge_data = net.get_edge_data(coms[0], coms[1])
+                if edge_data[0]["value"].split(":")[-1] != proto:
+                    net.add_edge(coms[0], coms[1], value=protos)
+            else:
+                net.add_node(coms[0]) # add src_addr
+                net.add_node(coms[1]) # add dst_addr
+                net.add_edge(coms[0], coms[1], value=proto)
+    net_graph = Network(directed=True)
+    net_graph.from_nx(net)
+    net_graph.set_edge_smooth('dynamic')
+    net_graph.toggle_physics(False)
+    net_graph.show_buttons()
+    net_graph.show("./test.html", notebook=False)
+    # net.show("./test.html", notebook=False)
 
 def main() -> None:
     data = read_cap()
-    net = Network(bgcolor="#222222", font_color="white")
+    net = nx.MultiDiGraph()
+    # net = Network(bgcolor="#222222", font_color="white")
     make_nodes(net, data)
 
 if __name__ == "__main__":
